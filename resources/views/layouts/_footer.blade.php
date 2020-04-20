18cbbbb3252c4f7cd25df74fc0607b9e55f67487
zane <zhichao1533>
2020-04-20 02:31:58 -0700

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+	<a href="https://learnku.com/laravel/courses" target="_blank">
+		可以俩你想，每日精进</a>
+	<div class="float-right">
+		<a href="/about">关于</a>
+	</div>
+</footer>
