cd2a1995c8f26fcb1edf77256ff9ed10219606d7
zane <zhichao1533>
2020-04-22 02:41:46 -0700

    创建用户查询信息

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,15 @@
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
-Route::get('/signup','UsersController@create')->name('signup');
+
+Route::get('signup','UsersController@create')->name('signup');
+Route::resource('users','UsersController');
+/**上面创建的这句话相当于
+ * te::get('/users','UsersController@index')->name('users.index'); 显示所有用户列表的页面
+ * Route::get('/users/create','UsersController@create')->name('users.create'); 显示用户个人信息的页面
+ * Route::get('/users/{user}','UsersController@show')->name('user.show'); 创建用户的页面
+ * Route::post('/users','UsersController@store')->name('users.store');  创建用户
+ * Route::get('/users/{user}/edit',"UsersControll;er@edit")->name('users.edit');编辑用户个人资料的页面
+ * Route::patch('/users/{user}','UsersController@update')->name('user.update');更新用户 
+ * Route::delete('/users/{user}','UsersController@destroy')->name('users,destroy'); 删除用户
+ */
