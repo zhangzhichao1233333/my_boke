ed611e894e0adc3d98fb805334131e7a841b0c67
zane <zhichao1533>
2020-04-22 02:53:45 -0700

    测试ompact的作用

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,8 +6,23 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+	/** 创建
+	 * @function create 
+	 * @author Zane
+	 * @data 2020/4/22
+	**/ 
 	public function create() 
 	{
 		return view('users.create');
+	}
+	/**展示用户信息
+	 * @function show
+	 * @author Zane
+	 * @data 2020/4/22
+	 * @param Object $user  
+	 */
+	pubilc function show(User $user){
+		echo"<pre>";print_r(compact('user'));exit;
+		return view('users.show',compact('user'));		
 	}
 }
