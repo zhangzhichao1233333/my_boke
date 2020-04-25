e6bab75cdebcbb49ccc283fedd5ead6f24570f13
zane <zhichao1533>
2020-04-25 04:13:35 -0700

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -48,6 +48,6 @@ class UsersController extends Controller
 			'password' => bcrypt($request->password),
 		]);
 		session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-		return tedirect()->route('users.show',[$user]);
+		return redirect()->route('users.show',[$user]);
 	}
 }
