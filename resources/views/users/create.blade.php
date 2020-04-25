0ac92f7867dbd5e98a231f6e1246b6017fa4971f
zane <zhichao1533>
2020-04-24 21:14:52 -0700

    增加 错误提示

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,7 +9,7 @@
 		</div>
 		<div class="card-body">
 			@include('shared._errors')
-			<from method="POST" action="{{ route('users.store') }}">
+			<form method="POST" action="{{ route('users.store') }}">
 				{{ csrf_field() }}
 				<div class="form-group">
 					<label for="name">名称：</label>
