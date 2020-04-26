0ce8b4e49bf23ffb705fce11c0928186f7771a83
zane <zhichao1533>
2020-04-26 03:46:02 -0700

    创建会话

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Moldes\User::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
