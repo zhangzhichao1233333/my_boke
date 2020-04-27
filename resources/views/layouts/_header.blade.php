7b2a53dbaac3461589587c0890da435efa00b0d8
zane <zhichao1533>
2020-04-27 00:33:29 -0700

    登录成功后导航逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,7 +9,7 @@
                         {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('users.show',Auth::user()) }} ">
+                                <a class="dropdown-item" href="{{ route('users.show',Auth::user()) }} ">个人中心</a>
                                 <a class="dropdown-item" href="#">编辑资料</a>
                                 <div class="dropdown-divider" ></div>
                                 <a class="dropdown-item" id="logout" href="#">
