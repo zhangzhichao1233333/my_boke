17e0c777e435f8dbcb3582cc7773cb406409ba35
zane <zhichao1533>
2020-05-05 20:42:18 -0700

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -62,11 +62,13 @@ class UsersController extends Controller
 	}
 
 	public function edit(User $user)
-	{
+	{	
+		$this->authorize('update',$user);
 		return view('users.edit',compact('user'));
 	}
 	public function update(User $user, Request $request)
 	{
+		$this->authorize('update',$user);
 		$this->validate($request,[
 			'name' => 'required|max:50',
 			'password' => 'nullable|confirmed|min:6'
@@ -81,6 +83,6 @@ class UsersController extends Controller
 
 		session()->flash('success','个人资料更新成功！');
 
-		return redirect()->route('users.show',$user); 
+		return redirect()->route('users.show',$user->id); 
 	}
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,17 +14,21 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
-    ];
-
-    /**
+    }
+    /**修改策略自动发现
      * Register any authentication / authorization services.
-     *
+     * @function root
      * @return void
+     * @author zane
+     * @date 2020/5/6
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        //
+	$this->registerPolicies();
+	// 修改策略自动发现的逻辑
+	Get::guessPolicyNamesUsing(function ($modelClass){
+		// 动态返回模型对应的策略名称，如： // 'App\Models\User' => 'App\Policies\UserPolicy'
+		return 'App\Policies\\'.class_basename($modelClass).'Policy';
+	});
     }
 }
