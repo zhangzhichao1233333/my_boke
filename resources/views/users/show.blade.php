2dad9557c9de3626e6ed9c068f21fec487546071
zane <zhichao1533>
2020-05-08 02:16:15 -0700

    用户微博列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="row">
 	<div class="offset-md-2 col-md-8">
-		<div class="col-md-12">
-			<div class="offset-md2 col-md-8">
+		<!--div class="col-md-12"-->
+			<!--div class="offset-md2 col-md-8"-->
 				<section class="user_info">
 					@include('shared._user_info',['user'=> $user])
 				</section>
@@ -22,8 +22,8 @@
 					<p>没有数据</p>
 					@endif
 				</section>
-			</div>
-		</div>
+			<!--/div-->
+		<!--/div-->
 	</div>
 </div>
 @stop
