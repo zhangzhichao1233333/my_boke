4976015d4f7c51c2027416a03e46cd45265c1efd
zane <zhichao1533>
2020-05-14 02:37:40 -0700

    基础页面结构

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -5,7 +5,7 @@
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span></button>
 		
-		<div class="collapse navnar-collapse" id="navbarSupportedContent">
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<!-- Left side Of navbar -->
 			<ul class="navbar-nav mr-auto">
 			</ul>
