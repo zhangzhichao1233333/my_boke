435c48a6da6e257f088feb6694901b6fbd8ff34d
zane <zhichao1533>
2020-05-14 23:36:33 -0700

    修复跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,30 +11,35 @@
 			</ul>
 
 			<!-- Right side Of Navbar -->
-                	<ul class="navbar-nav navbar-right">
-                	@if (Auth::check())
-                        	<li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">用户列表</a></li>
-                        	<li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        		{{ Auth::user()->name }}
-                        	</a>
-                        	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                	<a class="dropdown-item" href="{{ route('users.show',Auth::user()) }} ">个人中心</a>
-                                	<a class="dropdown-item" href="{{ route('users.edit', Auth::user() )}}">编辑资料</a>
-                                	<div class="dropdown-divider" ></div>
-                                	<a class="dropdown-item" id="logout" href="#">
-                                        	<form action ="{{ route('logout') }}" method="POST">
-                                                	{{ csrf_field() }}
-                                                	{{ method_field('DELETE')}}
-                                                	<button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                                        	</form>
-                               		</a>
-                        	</div>
-                        	</li>
-                	@else
-                	 <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-	<li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
-			@endif
+			<ul class="navbar-nav navbar-right">
+				<!--Authentication Links -->
+				@guest
+				<li class="nav-item">
+					<a class="nav-link" href="{{ route('login') }}">登录</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="{{ route('register')}}">注册</a>
+				</li>
+				@else
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img src="https:://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+					{{ Auth:user()->name }}
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="">个人中心</a>
+						<a class="dropdown-ttem" href="">编辑资料</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" id="logout" href="#">
+							<form action="{{ route('logout') }}" method="POST">
+								{{ csrf_field() }}
+								<button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+							
+							</form>
+						</a>
+					</div>
+				</li>
+				@endguest
                 	</ul>
 		</div>
         </div>
