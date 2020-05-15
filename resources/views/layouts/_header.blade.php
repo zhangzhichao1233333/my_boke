b35401cfa6bb90de3d26a1436f4bd016797956ca
zane <zhichao1533>
2020-05-15 00:28:12 -0700

    修复跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
-<img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px"/>
+<img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"/>
 
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
 <div class="container">
     <!-- Branding Image -->
     <a class="navbar-brand " href="{{ url('/') }}">
