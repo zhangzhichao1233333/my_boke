31bc33da1076760a0c24090a3a009691966f6419
zane <zhichao1533>
2020-05-14 20:50:39 -0700

    声场用户认证代码

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,34 +1,47 @@
-@extends('layouts.default')
-@section('title','重置密码')
+@extends('layouts.app')
 
 @section('content')
-<div class="col-md-8 offset-md-2">
-	<div class="card">
-		<div class="card-header"><h5>重置密码</h5></div>
-		
-		<div class="card-body">
-			@if (session('status'))
-			<div class="alert alert-success">
-				{{ session('status') }}	
-			</div>
-			@endif
-			<form class="" method="POST" action="{{ route('password.email') }}">
-				{{ csrf_field() }}
-				<div class="form-group{{ $errors->has('email')?'has-error' :'' }}">
-					<label for="email" class="form-control-label">邮箱地址:</label>
-					<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-					@if ($errors->has('email'))
-					<span class="form-text">
-						<strong>{{ $errors->first('email') }}</strong>
-					</span>
-					@endif
-				</div>
-			
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary">发送密码充值邮件</button>
-				</div>
-			</form>
-		</div>
-	</div>
-</div> 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Reset Password') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Send Password Reset Link') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
