5beb0b4b26eb06bfffe7a909228ad8ba28bdf6fe
zane <zhichao1533>
2020-05-26 18:07:59 -0700

    编辑个人资料

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,7 +26,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-		'name'  	=> 'required|between:3,25|rehex:/^[A-Za-z0-9-\_]+$/|unique:users,name,' . Auth::id(),
+		'name'  	=> 'required|between:3,25|rehex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
 		'email' 	=> 'required|email',
 		'introduction' 	=> 'max:80',
         ];
