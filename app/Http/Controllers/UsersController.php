fc69120bde1009ded252cdc5348031f3bb62d337
zane <zhichao1533>
2020-05-27 18:33:27 -0700

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -107,6 +107,7 @@ class UsersController extends Controller
 //			$data['password'] = bcrypt($request->password);
 //		}
 //		$user->update($data);	
+		dd($request->avatar);
 		$user->update($request->all());
 		return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
 //		session()->flash('success','个人资料更新成功！');
