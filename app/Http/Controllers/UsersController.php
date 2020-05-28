911aa78e8023c8cab3bf0dbeafb6342c578130f4
zane <zhichao1533>
2020-05-28 03:47:04 -0700

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -95,7 +95,7 @@ class UsersController extends Controller
 		$this->authorize('update',$user);
 		return view('users.edit',compact('user'));
 	}
-	public function update(UserRequest $request, User $user)
+	public function update1(UserRequest $request, User $user)
 //	public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
 	{
 		dd($request->avatar);
