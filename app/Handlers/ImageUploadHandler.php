4541c95df845e05bee24ad0b137ac6b6b71cf076
zane <zhichao1533>
2020-05-27 21:49:43 -0700

    上传头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -35,7 +35,8 @@ class ImageUploadHandler
         $file->move($upload_path, $filename);
 
         return [
-            'path' => config('app.url') . "/$folder_name/$filename"
-        ];
+//            'path' => config('app.url') . "/$folder_name/$filename"
+		'path' => "/$folder_name/$filename"
+	];
     }
 }   
