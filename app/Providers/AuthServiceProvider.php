931e6d22d438dbd3505b540593ea2d744c62a8d0
zane <zhichao1533>
2020-05-28 03:11:55 -0700

    剪裁头像

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+//use Illuminate\Support\Facades\Gate;
+
+use Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
