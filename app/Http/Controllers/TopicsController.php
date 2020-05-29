f5d2380227a9ff4c2a3c2f5518708f43400e2008
zane <zhichao1533>
2020-05-29 00:44:11 -0700

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
