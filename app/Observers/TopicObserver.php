d377bb5a35c0fd42444a848e953390d767f69d45
zane <zhichao1533>
2020-06-03 09:47:37 -0700

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,7 +22,7 @@ class TopicObserver
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
-	echo"<pre>";print_r($topic);die;
+
         $topic->excerpt = make_excerpt($topic->body);
     }    
 }
