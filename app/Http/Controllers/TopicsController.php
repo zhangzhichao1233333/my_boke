adf6623152fc9851f031da0574e5bec730ef4d38
zane <zhichao1533>
2020-06-02 20:43:40 -0700

    用户可以上传话题图片

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -73,7 +73,7 @@ class TopicsController extends Controller
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
 
-	public function uploadImage111(Request $request, ImageUploadHandler $uploader)
+	public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
         // 初始化返回数据，默认是失败的
         $data = [
