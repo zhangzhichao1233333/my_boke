05886d6eeb117491ea121c9f9eea1d4ba686a692
zane <zhichao1533>
2020-06-02 23:57:05 -0700

    用户可以上传话题图片

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -29,9 +29,6 @@ class ImageUploadHandler
         // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID 
         // 值如：1_1493521050_7BVc9v9ujP.png
         $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
-	echo"<pre>";print_r($extension);
-	echo"<pre>";print_r($this->allowed_ext);
-	echo"<pre>";print_r(in_array($extension, $this->allowed_ext));die;
         // 如果上传的不是图片将终止操作
         if ( ! in_array($extension, $this->allowed_ext)) {
             return false;
