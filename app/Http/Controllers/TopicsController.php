4dec63c9b506c2739d612919398e53a3dba99f2a
zane <zhichao1533>
2020-06-03 08:30:46 -0700

    修复 XSS 注入漏洞

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -63,7 +63,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功！');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
