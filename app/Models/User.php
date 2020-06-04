016e0bb5e8296da504222f39f7b6ea17278b1faf
zane <zhichao1533>
2020-06-03 23:06:15 -0700

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->hasMany(Topic::class);
     }
 
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+
    /* public function gravatar($size = '100')
     {
     	$hash = md5(strtolower(trim($this->attributes['email'])));
