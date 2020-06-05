6d99dd35261bf069108c7b94bc41fec557299e8c
zane <zhichao1533>
2020-06-05 02:45:26 -0700

    回复数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+	$this->call(UsersTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
 	$this->call(RepliesTableSeeder::class);
-	$this->call(TopicsTableSeeder::class);
     }
 }
