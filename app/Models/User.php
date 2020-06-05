0e7bb8f1b9f6ed92dd000e5c006883475da8562f
zane <zhichao1533>
2020-06-05 02:18:38 -0700

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,63 +48,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->id == $model->user_id;
     }
 
-   /* public function gravatar($size = '100')
+    public function replies()
     {
-    	$hash = md5(strtolower(trim($this->attributes['email'])));
-	return "http://www.gravatar.com/avatar/$hash?s=$size";	
+        return $this->hasMany(Reply::class);
     }
-
-    public static function boot()
-    {
-    	parent::boot();
-
-	static::creating(function ($user) {
-		$user->activation_token = Str::random(10);
-	});
-    }
-
-    public function statuses()
-    {
-    	return $this->hasMany(Status::class);
-    }
-
-    public function feed()
-    {
-        $user_ids = $this->followings->pluck('id')->toArray();
-        array_push($user_ids, $this->id);
-        return Status::whereIn('user_id', $user_ids)
-                              ->with('user')
-                              ->orderBy('created_at', 'desc');
-    }
-
-    public function followers()
-    {
-    	return $this->belongsToMany(User::Class,'followers','user_id','follower_id');
-    }
-	
-    public function followings()
-    {
-    	return $this->belongsToMany(User::Class,'followers','follower_id','user_id');
-    }
-
-    public function follow($user_ids)
-    {
-        if ( ! is_array($user_ids)) {
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->sync($user_ids, false);
-    }
-
-    public function unfollow($user_ids)
-    {
-        if ( ! is_array($user_ids)) {
-            $user_ids = compact('user_ids');
-        }
-        $this->followings()->detach($user_ids);
-    }
-
-    public function isFollowing($user_id)
-    {
-        return $this->followings->contains($user_id);
-    }*/
 }
--- routes/web.php
+++ routes/web.php
@@ -84,3 +84,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //设置上传图片的 URL
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
