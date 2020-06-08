8401b40f7d93870a7062b6baa777e252f06be6c4
zane <zhichao1533>
2020-06-08 00:13:14 -0700

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -50,7 +50,7 @@ class TopicReplied extends Notification implements ShouldQueue
         $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
 
         return (new MailMessage)
-                    ->line('你的话题有新回复！i11')
+                    ->line('你的话题有新回复哟!')
                     ->action('查看回复', $url);
     }
 
