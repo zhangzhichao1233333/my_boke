e6481a2ce63c1bd56764cedee39260b0e3953ac7
zane <zhichao1533>
2020-06-07 18:59:41 -0700

    支持队列的邮件通知

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -54,7 +54,7 @@ class Topic extends Model
 
     public function link($params = [])
     {
-        print_r(route('topics.show'));die;
+        print_r(route('topics.show', array_merge([$this->id, $this->slug], $params)));die;
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
 }
