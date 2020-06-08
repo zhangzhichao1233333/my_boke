70829bf48152b5fefcf56da0e69ca4685c6f5c86
zane <zhichao1533>
2020-06-07 18:09:15 -0700

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -75,4 +75,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
         $this->laravelNotify($instance);
     }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
