7c4b5a0f504c54fb0204d84d6ca9b62ada5ce48c
zane <zhichao1533>
2020-06-08 01:08:32 -0700

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -39,5 +39,10 @@ class TopicObserver
             // 推送任务到队列
             dispatch(new TranslateSlug($topic));
         }
+   }
+
+   public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
     } 
 }
