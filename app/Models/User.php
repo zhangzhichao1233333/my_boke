4a69fa639efe8113c8a1f91879c65e196e774b72
Zane <247844450@qq.com>
2020-06-10 18:39:29 -0700

    集成 administrator

--- app/Models/User.php
+++ app/Models/User.php
@@ -86,4 +86,16 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
 }
