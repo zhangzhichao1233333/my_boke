9e76404242f6edaf7c9e43968b6da6e3743d5a63
Zane <247844450@qq.com>
2020-06-10 22:35:18 -0700

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -28,10 +28,11 @@ class TopicsController extends Controller
 	public function index(Request $request,Topic $topic,User $user)
 	{
 		$topics = $topic->withOrder($request->order)
-			->with('user','category')
-			->paginate(20);
-		$active_users = $user->getActiveUsers();
-		return view('topics.index', compact('topics'));
+                        ->with('user', 'category')  // 预加载防止 N+1 问题
+                        ->paginate(20);
+        	$active_users = $user->getActiveUsers();
+      
+        	return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
