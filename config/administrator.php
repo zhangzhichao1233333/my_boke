c5e22493a7447a1b2c36106e335ea407c2841f30
Zane <247844450@qq.com>
2020-06-10 19:42:11 -0700

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -64,6 +64,11 @@ return array(
             'users',
             'roles',
             'permissions',
+    	],
+	'内容管理' => [
+            'categories',
+            'topics',
+            'replies',
         ],
     ],
 
